a1f696aa8e0079c02ae4d6e3b440b9818b34e110
PINKCONCRETE <155738325@qq.com>
2025-09-26 19:29:59 +0800

    Fix PPO on CUDA

--- CarPole_Demo/ppo_CarPole.py
+++ CarPole_Demo/ppo_CarPole.py
@@ -3,22 +3,21 @@ import gymnasium as gym
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 
-import os
-
 import torch
 
+print(torch.cuda.is_available())
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # Parallel environments
 vec_env = make_vec_env("CartPole-v1", n_envs=4)
 
 model = PPO("MlpPolicy", vec_env, verbose=1, device=device)
-model.learn(total_timesteps=25000)
-model.save("ppo_cartpole")
+model.learn(total_timesteps=100000)
+model.save("CarPole_Demo/ppo_cartpole")
 
 del model # remove to demonstrate saving and loading
 
-model = PPO.load("ppo_cartpole")
+model = PPO.load("CarPole_Demo/ppo_cartpole")
 
 obs = vec_env.reset()
 while True:
